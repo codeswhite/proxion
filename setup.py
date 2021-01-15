102b48c95133b3305d3582dac98ed1e32b294067
Max G <max3227@gmail.com>
2021-01-15 02:03:02 +0200

    Added a package setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxion",
+    version="1.4.0",
+    description="Multi-threaded proxy checker with uptime and latency statistics",
+    url="https://github.com/codeswhite/proxion",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'interutils',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'proxion = proxion:main',
+        ],
+    },
+    author="Max G",
+    author_email="max3227@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages()
+)
