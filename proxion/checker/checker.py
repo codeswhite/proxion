5490aa1d920c60587dc2fbedaed5569ae0413aed
Max G <max3227@gmail.com>
2021-07-04 10:01:05 +0300

    Improved concurrency and cleanup

--- proxion/checker/checker.py
+++ proxion/checker/checker.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import choice
 from time import sleep, time
 from typing import Iterable, Dict, List, Tuple
 from json.decoder import JSONDecodeError
@@ -21,7 +21,11 @@ urllib3.disable_warnings()
 
 class ProxyChecker:
     '''
-    Manages the whole process of checking
+    Manages the whole process of checking:
+
+    Proxy checked first receives an array of parameters
+    Then we aggregate shuffle and 
+
     '''
 
     def __init__(self, checklist: Iterable[Proxy],
@@ -31,32 +35,46 @@ class ProxyChecker:
                  no_shuffle: bool = False,
                  verbose: bool = False):
 
-        self.protocols = protocols
         self.timeout = timeout
         self.verbose = verbose
 
         if max_threads < 1:
             raise ValueError(f'Invalid thread count: {max_threads}')
-        if not self.protocols:
+        if not protocols:
             raise ValueError('Please specify at least one protocol!')
         if not checklist:
             raise ValueError('No proxies to check!')
 
-        if not no_shuffle:
-            shuffle(checklist)
+        # Aggregate, randomize and enqueue jobs (checks)
+        jobs = []
+        for proxy in checklist:
+            # Get proxies to check
+            protos_to_check = Defaults.checker_proxy_protocols
+            if proxy.protos:
+                protos_to_check = proxy.protos
+            protos_to_check = [
+                p for p in protos_to_check if p in protocols]
 
-        max_threads = min(max_threads, len(checklist))
-        pr(f'Checking {cyan(len(checklist))} proxies on {cyan(max_threads)} threads')
-
-        self._terminate_flag = False
+            for proto in protos_to_check:
+                # Randomize job list
+                pos = 0
+                jobs_count = len(jobs)
+                if not no_shuffle and jobs_count > 1:
+                    pos = choice(range(jobs_count))
+                jobs.insert(pos, (proxy, proto))
 
         self.queue = mp.Queue()
-        for proxy in checklist:
-            self.queue.put(proxy)
+        for job in jobs:
+            self.queue.put(job)
 
+        jobs_count = len(jobs)
+        max_threads = min(max_threads, jobs_count)
+        pr(f'Checking {cyan(len(checklist))} proxies ({cyan(jobs_count)} jobs) on {cyan(max_threads)} threads')
+
+        self._terminate_flag = False
         with mp.Manager() as manager:
             self.up = manager.list()
-            self.down = manager.list()
+            self.jobs_done = manager.Value('i', 0)
 
             self.procs = []
             for _ in range(max_threads):
@@ -70,9 +88,9 @@ class ProxyChecker:
             try:
                 while self.active_children():
                     sleep(0.25)
-                    # print('Running: ', self.active_children())
-                    if time() - last_print > print_interval:
+                    if self.verbose and time() - last_print > print_interval:
                         last_print = time()
+                        pr(f'Jobs done: [{self.jobs_done.value}/{jobs_count}] = {100 * int(self.jobs_done.value) / jobs_count}%', '*')
                         self.show_status()
 
             except KeyboardInterrupt:
@@ -88,6 +106,8 @@ class ProxyChecker:
                     if time() - last_print > termination_print_interval:
                         last_print = time()
                         pr(f'Waiting for {cyan(n_alive)} children to exit', '*')
+                        pr(f'Jobs done: [{self.jobs_done.value}/{jobs_count}] = {100 * int(self.jobs_done.value) / jobs_count}%', '*')
+
             finally:
                 # pr('Joining remaining procs')
                 # pool.join()
@@ -101,36 +121,30 @@ class ProxyChecker:
         return list(map(lambda p: p.is_alive(), self.procs)).count(True)
 
     def worker(self):
-        while not self._terminate_flag and not self.queue.empty():
-            proxy: Proxy = self.queue.get()
+        while not self.queue.empty():
+            if self._terminate_flag:
+                pr('Terminating child although queue is not empty yet', '!')
+                break
+
+            proxy, proto = self.queue.get()
+            proxy: Proxy
+            proto: str
+
             if self.verbose:
-                pr(f'Thread {cyan(os.getpid())} checking: {cyan(proxy.pip)}', '*')
+                pr(f'Thread {cyan(os.getpid())} checking: {cyan(proxy.pip)} for proto: {cyan(proto)}', '*')
 
-            protos_to_check = Defaults.checker_proxy_protocols
-            if proxy.protos:
-                protos_to_check = proxy.protos
-            protos_to_check = [
-                p for p in protos_to_check if p in self.protocols]
-
-            for proto in protos_to_check:
-                res = self.perform_check(proxy.pip, proto)
-                if res is not None:
-                    # if self.verbose:
-                    pr(f'Working {cyan(", ".join(res.protos))} proxy @ {colored(proxy.pip, "green")}')
-                    self.up.append(res)
-                    break
-            else:
-                # if self.verbose:
-                pr(f'{cyan(proxy.pip)} -> is down!')
-                self.down.append(proxy.pip)
+            res = self.perform_check(proxy.pip, proto)
+            self.jobs_done.value += 1
+            if res is not None:
+                pr(f'Working {cyan(", ".join(res.protos))} proxy @ {colored(proxy.pip, "green")}')
+                self.up.append(res)
+                break
 
     def perform_check(self, pip: str, protocol: str) -> (Proxy, None):
         try:
             proxies_dict = {
                 protocol if protocol == 'http' else 'https': protocol + '://' + pip
             }
-            if self.verbose:
-                pr(f'Proxy {pip}: Testing {protocol} proto', '*')
 
             _t = time()
             # Attempt to get our current IP (trough the proxy), expect JSON data!
@@ -141,7 +155,6 @@ class ProxyChecker:
                 # Attempt to decode the received data
                 json = resp.json()
                 try:
-                    # return Proxy(pip, protocol, json, t)
                     return Proxy(pip, (protocol,), time(), latency, json['country'])
                 except KeyError as err:
                     pr(f'Result parsing "{err}" from:' + json, '*')
@@ -175,13 +188,10 @@ class ProxyChecker:
             return dic
 
         ''' Show status (using the collected results) '''
-        working, down = self.up, self.down
+        working = self.up
         text = 'Working:'
-        protocols = _sort_protocols(working)
-        for proto, proxies in protocols.items():
+        for proto, proxies in _sort_protocols(working).items():
             if proxies:
                 text += f' {cyan(proto.upper())}:{cyan(len(proxies))}'
         pr(text)
-        pr('Down: ' + cyan(len(down)))
-        pr('Total proxies checked: ' + cyan(len(working) + len(down)))
         print()
